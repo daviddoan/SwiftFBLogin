75e1dc35dbe1e390c5c7b6f03afa89bc7b76b026
David Doan <ddoan17@mit.edu>
2015-04-04 03:44:57 -0400

    Initial Commit

---
+++ SwiftFBLogin/ViewController.swift
@@ -0,0 +1,98 @@
+//
+//  ViewController.swift
+//  SwiftFBLogin
+//
+//  Created by David Doan on 4/4/15.
+//  Copyright (c) 2015 David Doan. All rights reserved.
+//
+
+import UIKit
+class ViewController: UIViewController, FBSDKLoginButtonDelegate {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        if (FBSDKAccessToken.currentAccessToken() != nil)
+        {
+            // User is already logged in, do work such as go to next view controller.
+            
+            // Or Show Logout Button
+            let loginView : FBSDKLoginButton = FBSDKLoginButton()
+            self.view.addSubview(loginView)
+            loginView.center = self.view.center
+            loginView.readPermissions = ["public_profile", "email", "user_friends", "user_photos"]
+            loginView.delegate = self
+            self.returnUserData()
+        }
+        else
+        {
+            let loginView : FBSDKLoginButton = FBSDKLoginButton()
+            self.view.addSubview(loginView)
+            loginView.center = self.view.center
+            loginView.readPermissions = ["public_profile", "email", "user_friends","user_photos"]
+            loginView.delegate = self
+        }
+        
+    }
+    
+    // Facebook Delegate Methods
+    
+    func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError!) {
+        println("User Logged In")
+        
+        if ((error) != nil)
+        {
+            // Process error
+        }
+        else if result.isCancelled {
+            // Handle cancellations
+        }
+        else {
+            // If you ask for multiple permissions at once, you
+            // should check if specific permissions missing
+            if result.grantedPermissions.containsObject("email")
+            {
+                // Do work
+            }
+            
+            self.returnUserData()
+        }
+        
+    }
+    
+    func loginButtonDidLogOut(loginButton: FBSDKLoginButton!) {
+        println("User Logged Out")
+    }
+    
+    func returnUserData()
+    {
+        let graphRequest : FBSDKGraphRequest = FBSDKGraphRequest(graphPath: "me", parameters: nil)
+        graphRequest.startWithCompletionHandler({ (connection, result, error) -> Void in
+            
+            if ((error) != nil)
+            {
+                // Process error
+                println("Error: \(error)")
+            }
+            else
+            {
+                println("fetched user: \(result)")
+                let userName : NSString = result.valueForKey("name") as NSString
+                println("User Name is: \(userName)")
+                let userEmail : NSString = result.valueForKey("email") as NSString
+                println("User Email is: \(userEmail)")
+                let userid: NSString = result.valueForKey("id") as NSString
+                println("Picture is: http://graph.facebook.com/\(userid)/picture?type=large")
+            }
+        })
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
